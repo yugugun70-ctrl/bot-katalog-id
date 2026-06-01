3cfa7e41d2710f7d1e814396c62dc7f667741543
yugugun70-ctrl <yugugun70@gmail.com>
2026-06-01 17:20:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-const express = require("express");
 const { Telegraf } = require("telegraf");
-
-const app = express();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// command
 bot.start((ctx) => {
-  ctx.reply("TES NX CREATE");
+  ctx.reply("Bot aktif di Termux 🚀");
 });
 
 bot.on("text", (ctx) => {
   ctx.reply(`Kamu bilang: ${ctx.message.text}`);
 });
 
-// webhook route
-app.use(bot.webhookCallback("/webhook"));
+bot.launch();
 
-app.get("/", (req, res) => {
-  res.send("Bot hidup");
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, async () => {
-  const url = process.env.WEBHOOK_URL;
-
-  await bot.telegram.setWebhook(`${url}/webhook`);
-
-  console.log("Bot webhook aktif:", url);
-});
+console.log("Bot jalan...");
