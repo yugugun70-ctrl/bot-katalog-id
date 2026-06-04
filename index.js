e238fec27db32c193f308bd4087f8816cdbedf79
yugugun70-ctrl <yugugun70@gmail.com>
2026-06-04 07:59:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ bot.start((ctx) => {
   ctx.reply("Bot aktif di Termux 🚀");
 });
 
+// 👇 COMMAND /open (SUDAH DITAMBAHKAN)
+bot.command("open", (ctx) => {
+  ctx.reply("Klik untuk lanjut:", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "🚀 Lanjut",
+            url: "https://yugugun70-ctrl.github.io/telegram-open/"
+          }
+        ]
+      ]
+    }
+  });
+});
+
 bot.on("text", (ctx) => {
   ctx.reply(`Kamu bilang: ${ctx.message.text}`);
 });
